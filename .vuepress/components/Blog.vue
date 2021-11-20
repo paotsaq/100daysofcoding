aaeb8a89a51f205e7bfdd0b06434b90a8b78461c
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-11-20 15:53:04 +0000

    starts work on layout

--- .vuepress/components/Blog.vue
+++ .vuepress/components/Blog.vue
@@ -1,10 +1,13 @@
 <template>
 <div>
-    <div v-for="post in posts">
-        <h2>
-            <router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
-        </h2>
-        <p>{{ post.frontmatter.description }}</p>
+	<div v-for="post in posts">
+	<div>
+		<div class="postInfo">
+			<router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
+			<time style="color: #2f855e; float:right; font-weight: 600"> {{ processedDate(post.frontmatter.date) }} </time>
+		</div>
+	</div>
+        <div style="margin-top: 0px;">{{ post.frontmatter.description }}</div>
     </div>
 </div>
 </template>
@@ -17,6 +20,22 @@ export default {
                 .filter(x => x.path.startsWith('/blog/') && !x.frontmatter.blog_index)
                 .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
         }
+    },
+    methods: {
+	processedDate(postDate) {
+		return postDate.split('T')[0].split('-').reverse().join('/')
+	}
     }
 }
 </script>
+
+<style scoped>
+.postInfo > a { 
+  font-size: 1.65rem;
+  padding-bottom: .3rem;
+  border-bottom: 1px solid #a7b1bc;
+  font-weight: 600;
+	line-height: 1.25;
+}
+
+</style>
