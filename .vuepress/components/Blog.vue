2f28e35080fefc57d43a3aebe889c48ad6597d31
apinto <senhoralexandrepinto@protonmail.com>
2022-03-29 15:14:04 +0100

    starts tagsSidebar

--- .vuepress/components/Blog.vue
+++ .vuepress/components/Blog.vue
@@ -1,107 +1,131 @@
 <template>
-	<div>
-		<div class="post" v-for="post in posts">
-			<div class="postInfo">
-				<router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
-				<time style=""> {{ processedDate(post.frontmatter.date) }} </time>
-			</div>
-			<div>{{ post.frontmatter.description }}</div>
-			<div class="postTags" v-if="'tags' in post.frontmatter">
-				<ul class="tag" v-for="tag in getTags(post.frontmatter.tags)">
-					<li>{{ tag }}</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+  <div class="pageDisplay">
+    <tagsFilter />
+	<div class="blogPosts">
+    <div class="post" v-for="post in posts">
+      <div class="postInfo">
+        <router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
+        <time style=""> {{ processedDate(post.frontmatter.date) }} </time>
+      </div>
+      <div>{{ post.frontmatter.description }}</div>
+      <div class="postTags" v-if="'tags' in post.frontmatter">
+        <ul class="tag" v-for="tag in getTags(post.frontmatter.tags)">
+          <li>{{ tag }}</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import tagsFilter from "./tagsFilter.vue";
+
 export default {
-    computed: {
-        posts() {
-            return this.$site.pages
-                .filter(x => x.path.startsWith('/blog/') && !x.frontmatter.blog_index)
-                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
-				}
-			  },
-		methods: {
-			 processedDate(postDate) {
-				 return postDate.split('T')[0].split('-').reverse().join('/')
-				 },
-			 getTags(tagsLine) {
-				 return tagsLine.split(',').map(x => x.trim()).filter(x =>x.length > 0)
-				 }
-			 }
-}
+  components: {
+    tagsFilter,
+  },
+  computed: {
+    posts() {
+      return this.$site.pages
+        .filter((x) => x.path.startsWith("/blog/") && !x.frontmatter.blog_index)
+        .sort(
+          (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+        );
+    },
+  },
+  methods: {
+    processedDate(postDate) {
+      return postDate.split("T")[0].split("-").reverse().join("/");
+    },
+    getTags(tagsLine) {
+      return tagsLine
+        .split(",")
+        .map((x) => x.trim())
+        .filter((x) => x.length > 0);
+    },
+  },
+};
 </script>
 
 <style scoped>
-	.post {
-	  margin-bottom: 20px;
-	}
 
-	.postInfo {
-	  border-bottom: 1px solid #a7b1bc;
-	  display: flex;
-	  margin-block-start: 0.83em;
-	  margin-block-end: 0.83em;
-	  margin-inline-start: 0px;
-	  margin-inline-end: 0px;
-	  padding-bottom: .3rem;
-	}
+.pageDisplay {
+	display: flex;
+	flex-direction: row;
+}
 
-	.postInfo > a { 
-	  font-size: 1.65rem;
-	  font-weight: 600;
-	  line-height: 1.25;
-	  width: 80vw;
-	  margin-right: 40px;
-	}
+.blogPosts {
+	display: flex;
+	flex-direction: column;
+	max-width: 740px;
+}
+.post {
+  margin-bottom: 20px;
+}
 
-	.postInfo > time { 
-	  display: inline-block;
-	  align-self: flex-end;
-	  color: #38a263;
-	  font-weight: 400;
-	}
+.postInfo {
+  border-bottom: 1px solid #a7b1bc;
+  display: flex;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-bottom: 0.3rem;
+}
 
-	@media (prefers-color-scheme: dark) {
-		.postInfo > time {
-		  color: #eaecef;
-		}
-	}
+.postInfo > a {
+  font-size: 1.65rem;
+  font-weight: 600;
+  line-height: 1.25;
+  width: 80vw;
+  margin-right: 40px;
+}
 
-	.postTags {
-      list-style: none;
-      margin: 6px 0 0 0;
-	  display: inline-block;
-	  align-self: flex-end;
-	}
+.postInfo > time {
+  display: inline-block;
+  align-self: flex-end;
+  color: #38a263;
+  font-weight: 400;
+}
 
-	.tag {
-	  background: #eee;
-	  border-radius: 7px 0 0 7px;
-	  color: #999;
-	  display: inline-block;
-	  line-height: 20px;
-	  padding: 0 17px 0 20px;
-	  position: relative;
-	  margin: 0 10px 0 0;
-	  text-decoration: none;
-	  font-size: 11px;
-	  -webkit-transition: color 0.2s;
-	}
+@media (prefers-color-scheme: dark) {
+  .postInfo > time {
+    color: #eaecef;
+  }
+}
 
-	@media (prefers-color-scheme: dark) {
-		.tag {
-		  background: #383737;
-		  color: #d9ffd2;
-		  -webkit-transition: color 0.2s;
-		}
-	}
+.postTags {
+  list-style: none;
+  margin: 6px 0 0 0;
+  display: inline-block;
+  align-self: flex-end;
+}
 
-	li::marker {
-	  font-size: 150%;
-	  color: white;
-	}
+.tag {
+  background: #eee;
+  border-radius: 7px 0 0 7px;
+  color: #999;
+  display: inline-block;
+  line-height: 20px;
+  padding: 0 17px 0 20px;
+  position: relative;
+  margin: 0 10px 0 0;
+  text-decoration: none;
+  font-size: 11px;
+  -webkit-transition: color 0.2s;
+}
+
+@media (prefers-color-scheme: dark) {
+  .tag {
+    background: #383737;
+    color: #d9ffd2;
+    -webkit-transition: color 0.2s;
+  }
+}
+
+li::marker {
+  font-size: 150%;
+  color: white;
+}
 </style>
---
+++ .vuepress/components/tagsFilter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tags">
+	<div v-for="tagelement in results" class="tag">
+		<li>{{ tagelement }}</li>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tagsFilter",
+  data() {
+    return {
+      results: [
+	  "vue",
+	  "musings",
+	  "python",
+	  "css",
+	  "django",
+	  "flask",
+	  "haskell"
+	  ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.tag {
+  background: #eee;
+  border-radius: 7px 0 0 7px;
+  color: #999;
+  display: inline-block;
+  line-height: 20px;
+  padding: 0 17px 0 20px;
+  position: relative;
+  margin: 0 10px 0 0;
+  text-decoration: none;
+  font-size: 11px;
+  -webkit-transition: color 0.2s;
+}
+
+.tags {
+  padding-top: 40px;
+  padding-left: 40px;
+  top: 0;
+  left: 0;
+}
+
+@media (prefers-color-scheme: dark) {
+  .tag {
+    background: #383737;
+    color: #d9ffd2;
+    -webkit-transition: color 0.2s;
+  }
+}
+
+li::marker {
+  font-size: 150%;
+  color: white;
+}
+</style>
